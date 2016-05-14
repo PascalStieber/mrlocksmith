d46cb552a59c0d9c7961cf17b95df610ead13195
pasca <pasca@DESKTOP-QNCN2TH.fritz.box>
2016-05-14 20:40:46 +0200

    initial

---
+++ mrlocksmith-server-zuul/src/main/java/com/pascalstieber/mrlocksmith/zuulserver/ZuulServiceApplication.java
@@ -0,0 +1,14 @@
+package com.pascalstieber.mrlocksmith.zuulserver;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulServiceApplication {
+
+    public static void main(String[] args) {
+	new SpringApplicationBuilder(ZuulServiceApplication.class).web(true).run(args);
+    }
+}
