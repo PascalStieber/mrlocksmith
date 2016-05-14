c5a46b5c27a6ac678f0daed7b62faa872501b763
pasca <pasca@DESKTOP-QNCN2TH.fritz.box>
2016-05-14 22:09:34 +0200

    test push to test jenkins build trigger #5

--- mrlocksmith-server-zuul/src/main/java/com/pascalstieber/mrlocksmith/zuulserver/ZuulServiceApplication.java
+++ mrlocksmith-server-zuul/src/main/java/com/pascalstieber/mrlocksmith/zuulserver/ZuulServiceApplication.java
@@ -11,4 +11,5 @@ public class ZuulServiceApplication {
     public static void main(String[] args) {
 	new SpringApplicationBuilder(ZuulServiceApplication.class).web(true).run(args);
     }
+    
 }
