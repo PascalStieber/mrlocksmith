2d2d7545ecf4f107749866e76681ceab09ea6c67
pasca <pasca@DESKTOP-QNCN2TH.fritz.box>
2016-09-14 20:52:47 +0200

    added last microservices

---
+++ mrlocksmith-quotation/src/main/java/com/pascalstieber/mrlocksmith/quotation/QuotationApp.java
@@ -0,0 +1,20 @@
+package com.pascalstieber.mrlocksmith.quotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+
+@ComponentScan
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@Component
+public class QuotationApp {
+
+    public static void main(String[] args) {
+	SpringApplication.run(QuotationApp.class, args);
+    }
+    
+    
+}
