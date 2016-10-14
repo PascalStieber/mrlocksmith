4b2e53b28a687b9a6f9241a5d475a2da326a95e2
pasca <pasca@DESKTOP-QNCN2TH>
2016-10-14 14:08:27 +0200

    initial branch changes

--- mrlocksmith-quotation/src/main/java/com/pascalstieber/mrlocksmith/quotation/QuotationApp.java
+++ mrlocksmith-quotation/src/main/java/com/pascalstieber/mrlocksmith/quotation/QuotationApp.java
@@ -1,24 +1,65 @@
 package com.pascalstieber.mrlocksmith.quotation;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.pascalstieber.mrlocksmith.quotation.data.Item;
-import com.pascalstieber.mrlocksmith.quotation.data.ItemRepository;
-import com.pascalstieber.mrlocksmith.quotation.data.Quotation;
-import com.pascalstieber.mrlocksmith.quotation.data.QuotationRepository;
-
-@ComponentScan
+@SpringBootApplication
+@RestController
 @EnableAutoConfiguration
 @EnableDiscoveryClient
-@Component
-public class QuotationApp {
+public class QuotationApp extends WebSecurityConfigurerAdapter {
 
+    private final Logger log = LoggerFactory.getLogger(QuotationApp.class);
+
+  
     public static void main(String[] args) {
 	SpringApplication.run(QuotationApp.class, args);
+    }
+
+   
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+	
+    	AuthenticationEntryPoint aep = new AuthenticationEntryPoint() {
+	    
+	    @Override
+	    public void commence(HttpServletRequest request, HttpServletResponse response,
+		    org.springframework.security.core.AuthenticationException authException) throws IOException, ServletException {
+		response.sendRedirect("http://192.168.99.100:8080/quotation/login");
+	    }
+	};
+	
+	http.antMatcher("/**").authorizeRequests().antMatchers( "/login").permitAll().anyRequest().authenticated().
+	and().formLogin().loginPage("/login").permitAll().
+	and().exceptionHandling().authenticationEntryPoint(aep).
+	and().csrf().disable();
+    }
+    
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	auth.jdbcAuthentication().dataSource(dataSource).usersByUsernameQuery("SELECT email,password,enabled from user where email=?")
+		.authoritiesByUsernameQuery("select email,role from user where email=?");
+
     }
 }
--- mrlocksmith-quotation/src/main/java/com/pascalstieber/mrlocksmith/quotation/web/QuotationController.java
+++ mrlocksmith-quotation/src/main/java/com/pascalstieber/mrlocksmith/quotation/web/QuotationController.java
@@ -1,9 +1,7 @@
 package com.pascalstieber.mrlocksmith.quotation.web;
 
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
@@ -12,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,115 +26,138 @@ import com.pascalstieber.mrlocksmith.quotation.data.ItemRepository;
 import com.pascalstieber.mrlocksmith.quotation.data.OrderEntity;
 import com.pascalstieber.mrlocksmith.quotation.data.Quotation;
 import com.pascalstieber.mrlocksmith.quotation.data.QuotationRepository;
+import com.pascalstieber.mrlocksmith.quotation.data.User;
 import com.pascalstieber.mrlocksmith.quotation.logic.QuotationService;
 
 @Controller
 public class QuotationController {
 
-    private static final String REDIRECT_ON_HOST = "redirect:http://192.168.99.100:8080/";
-    private static final String HOST_URL = "http://192.168.99.100:8080/";
+	private static final String REDIRECT_ON_HOST = "redirect:http://192.168.99.100:8080/";
+	private static final String HOST_URL = "http://192.168.99.100:8080/";
 
-    private final Logger log = LoggerFactory.getLogger(QuotationController.class);
+	private final Logger log = LoggerFactory.getLogger(QuotationController.class);
 
-    private QuotationRepository quotationRepository;
-    private RegisterClient registerClient;
-    private OrderClient orderClient;
-    private QuotationService quotationService;
-    private ItemRepository itemRepository;
+	private QuotationRepository quotationRepository;
+	private RegisterClient registerClient;
+	private OrderClient orderClient;
+	private QuotationService quotationService;
+	private ItemRepository itemRepository;
 
-    @Autowired
-    public QuotationController(QuotationRepository quotationRepository, RegisterClient registerClient, OrderClient orderClient,
-	    QuotationService quotationService, ItemRepository itemRepository) {
-	this.quotationRepository = quotationRepository;
-	this.registerClient = registerClient;
-	this.orderClient = orderClient;
-	this.quotationService = quotationService;
-	this.itemRepository = itemRepository;
-    }
-
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public ModelAndView index() {
-	List<OrderEntity> allOrders = orderClient.getAllOrders();
-	return new ModelAndView("showAllOrders", "orders", allOrders);
-    }
-
-    @RequestMapping(value = "/findAllQuotationsForOrder/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody List<Quotation> findAllQuotationsForOrder(@PathVariable("id") long id) {
-	List<Quotation> quotation = quotationRepository.getAllQuotationsForOrder(id);
-	return quotation;
-    }
-    
-    @RequestMapping(value = "/getSumOfQuotation/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody String getSumOfQuotation(@PathVariable("id") long id) {
-	String sum = itemRepository.getSumOfQuotation(quotationRepository.findOne(id));
-	return sum;
-    }
-    
-    @RequestMapping(value = "/findAllItemsByQuotationId/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody List<Item> findAllItemsByQuotationId(@PathVariable("id") long id) {
-	List<Item> items = itemRepository.findByQuotation(quotationRepository.findOne(id));
-	return items;
-    }
-
-    @RequestMapping(value = "/acceptQuotation/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public @ResponseBody Boolean acceptQuotation(@PathVariable("id") long id) {
-	Quotation quotation = quotationRepository.findOne(id);
-	quotation.setAccepted(true);
-	quotation.setAcceptedAt(new Date());
-	quotationRepository.save(quotation);
-	return true;
-    }
-    
-    @RequestMapping(value = "/formQuotation.html", method = RequestMethod.POST, params = "action=submitTender")
-    public ModelAndView submitTender(HttpServletRequest httpServletRequest) {
-	long orderid = Long.parseLong(httpServletRequest.getParameter("orderid"));
-	Quotation quotation = new Quotation();
-	quotation.setOrderid(orderid);
-	quotation.addItem(new Item());
-	return new ModelAndView("submitTender", "quotation", quotation);
-    }
-
-    @RequestMapping(value = "/formQuotation.html", method = RequestMethod.POST, params = "action=editTender")
-    public ModelAndView editTender(HttpServletRequest httpServletRequest) {
-	long orderid = Long.parseLong(httpServletRequest.getParameter("orderid"));
-	Quotation quotation = quotationRepository.findByOrderidAndContractorid(orderid, 1l).get(0);
-	return new ModelAndView("submitTender", "quotation", quotation);
-    }
-
-    @RequestMapping(value = "/formQuotation.html", method = RequestMethod.POST, params = "action=addItem")
-    public ModelAndView addItem(Quotation quotation, BindingResult bindingResult, HttpServletRequest httpServletRequest) {
-	quotation.addItem(new Item());
-	return new ModelAndView("submitTender", "quotation", quotation);
-    }
-
-    @RequestMapping(value = "/removeItem.html", method = RequestMethod.POST)
-    public ModelAndView removeItem(Quotation quotation, BindingResult bindingResult,
-	    HttpServletRequest httpServletRequest) {
-	if (quotation.getId() != 0) {
-	    long removeItemId = Long.parseLong(httpServletRequest.getParameter("removeItemId"));
-	    itemRepository.delete(removeItemId);
-	    quotation.setItems(itemRepository.findByQuotation(quotation));
+	@Autowired
+	public QuotationController(QuotationRepository quotationRepository, RegisterClient registerClient,
+			OrderClient orderClient, QuotationService quotationService, ItemRepository itemRepository) {
+		this.quotationRepository = quotationRepository;
+		this.registerClient = registerClient;
+		this.orderClient = orderClient;
+		this.quotationService = quotationService;
+		this.itemRepository = itemRepository;
 	}
-	return new ModelAndView("submitTender", "quotation", quotation);
-    }
 
-    @RequestMapping(value = "/formQuotation.html", method = RequestMethod.POST, params = "action=saveTender")
-    public ModelAndView saveTender(@Valid Quotation quotation, BindingResult bindingResult, HttpServletRequest httpServletRequest) {
-	if (!bindingResult.getAllErrors().isEmpty()) {
-	    return new ModelAndView("submitTender", "quotation", quotation);
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public ModelAndView index() {
+		List<OrderEntity> allOrders = orderClient.getAllOrders();
+		return new ModelAndView("showAllOrders", "orders", allOrders);
 	}
-	quotation.setContractorid(1l);
-	quotation = quotationRepository.save(quotation);
 
-	return new ModelAndView(REDIRECT_ON_HOST + "quotation/");
-    }
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ModelAndView login() {
+		return new ModelAndView("login");
+	}
 
-    @RequestMapping(value = "/formQuotation.html", method = RequestMethod.POST, params = "action=cancelTender")
-    public ModelAndView cancelTender(HttpServletRequest httpServletRequest) {
-	long orderid = Long.parseLong(httpServletRequest.getParameter("orderid"));
-	List<Quotation> quotation = quotationRepository.findByOrderidAndContractorid(orderid, 1l);
-	quotationRepository.delete(quotation.get(0));
-	return new ModelAndView(REDIRECT_ON_HOST + "quotation/");
-    }
+	@RequestMapping(value = "/auth", method = RequestMethod.GET)
+	public String authorization() {
+		User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		log.trace(">>>Logged in User: " + loggedInUser.getUserid() + " with id: " + loggedInUser.getId());
+
+		return "redirect:http://192.168.99.100:8080/quotation/";
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ModelAndView indexWithContractor(@PathVariable("id") long contractorid, HttpServletRequest req) {
+		List<OrderEntity> allOrders = orderClient.getAllOrders();
+		req.getSession().setAttribute("contractorid", contractorid);
+		return new ModelAndView("showAllOrders", "orders", allOrders);
+	}
+
+	@RequestMapping(value = "/findAllQuotationsForOrder/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Quotation> findAllQuotationsForOrder(@PathVariable("id") long id) {
+		List<Quotation> quotation = quotationRepository.getAllQuotationsForOrder(id);
+		return quotation;
+	}
+
+	@RequestMapping(value = "/getSumOfQuotation/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody String getSumOfQuotation(@PathVariable("id") long id) {
+		String sum = itemRepository.getSumOfQuotation(quotationRepository.findOne(id));
+		return sum;
+	}
+
+	@RequestMapping(value = "/findAllItemsByQuotationId/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody List<Item> findAllItemsByQuotationId(@PathVariable("id") long id) {
+		List<Item> items = itemRepository.findByQuotation(quotationRepository.findOne(id));
+		return items;
+	}
+
+	@RequestMapping(value = "/acceptQuotation/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Boolean acceptQuotation(@PathVariable("id") long id) {
+		Quotation quotation = quotationRepository.findOne(id);
+		quotation.setAccepted(true);
+		quotation.setAcceptedAt(new Date());
+		quotationRepository.save(quotation);
+		return true;
+	}
+
+	@RequestMapping(value = "/formQuotation.html", method = RequestMethod.POST, params = "action=submitTender")
+	public ModelAndView submitTender(HttpServletRequest httpServletRequest) {
+		long orderid = Long.parseLong(httpServletRequest.getParameter("orderid"));
+		Quotation quotation = new Quotation();
+		quotation.setOrderid(orderid);
+		quotation.addItem(new Item());
+		return new ModelAndView("submitTender", "quotation", quotation);
+	}
+
+	@RequestMapping(value = "/formQuotation.html", method = RequestMethod.POST, params = "action=editTender")
+	public ModelAndView editTender(HttpServletRequest httpServletRequest) {
+		long orderid = Long.parseLong(httpServletRequest.getParameter("orderid"));
+		Quotation quotation = quotationRepository.findByOrderidAndContractorid(orderid, 1l).get(0);
+		return new ModelAndView("submitTender", "quotation", quotation);
+	}
+
+	@RequestMapping(value = "/formQuotation.html", method = RequestMethod.POST, params = "action=addItem")
+	public ModelAndView addItem(Quotation quotation, BindingResult bindingResult,
+			HttpServletRequest httpServletRequest) {
+		quotation.addItem(new Item());
+		return new ModelAndView("submitTender", "quotation", quotation);
+	}
+
+	@RequestMapping(value = "/removeItem.html", method = RequestMethod.POST)
+	public ModelAndView removeItem(Quotation quotation, BindingResult bindingResult,
+			HttpServletRequest httpServletRequest) {
+		if (quotation.getId() != 0) {
+			long removeItemId = Long.parseLong(httpServletRequest.getParameter("removeItemId"));
+			itemRepository.delete(removeItemId);
+			quotation.setItems(itemRepository.findByQuotation(quotation));
+		}
+		return new ModelAndView("submitTender", "quotation", quotation);
+	}
+
+	@RequestMapping(value = "/formQuotation.html", method = RequestMethod.POST, params = "action=saveTender")
+	public ModelAndView saveTender(@Valid Quotation quotation, BindingResult bindingResult,
+			HttpServletRequest httpServletRequest) {
+		if (!bindingResult.getAllErrors().isEmpty()) {
+			return new ModelAndView("submitTender", "quotation", quotation);
+		}
+		quotation.setContractorid(1L);
+		quotation = quotationRepository.save(quotation);
+
+		return new ModelAndView(REDIRECT_ON_HOST + "quotation/");
+	}
+
+	@RequestMapping(value = "/formQuotation.html", method = RequestMethod.POST, params = "action=cancelTender")
+	public ModelAndView cancelTender(HttpServletRequest httpServletRequest) {
+		long orderid = Long.parseLong(httpServletRequest.getParameter("orderid"));
+		List<Quotation> quotation = quotationRepository.findByOrderidAndContractorid(orderid, 1l);
+		quotationRepository.delete(quotation.get(0));
+		return new ModelAndView(REDIRECT_ON_HOST + "quotation/");
+	}
 
 }
